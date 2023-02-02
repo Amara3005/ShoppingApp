701993943dc60c988220c63f8e89c3cc0e2aca0f
Amara3005 <amaraprasana@gmail.com>
2023-02-02 15:36:43 +0530

    Developed some code

--- src/main/java/com/ShoppingApplication/Application/Orders.java
+++ src/main/java/com/ShoppingApplication/Application/Orders.java
@@ -1,5 +1,21 @@
 package com.ShoppingApplication.Application;
 
+import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+
+@Entity(name="orders")
 public class Orders {
+	
+	@Id
+	private int orderId;
+	private String transactionId="";
+	private String status;
+	private String description="Transaction yet to complete";
+	private int amount;
+	private String Coupon;
+	private String date;
+	private int quantity;
+	
 
 }
